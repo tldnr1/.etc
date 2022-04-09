ed905a7ba9562dab02e3b87e2dbaaa3f4ee36900
RnL <63404135+tldnr1@users.noreply.github.com>
2022-04-09 10:01:40 +0000

    more about 'print'

---
+++ 파이썬_입문/python_print.py
@@ -0,0 +1,9 @@
+# link = https://gilu-world.tistory.com/40
+# print("출력할 내용", "여러개 적고 나서", \
+# end='문자열 끝에 뭐할지', sep='내용들 사이에 뭐할지')
+
+print('내용1', '내용2', end = ' 문자열 끝\n')
+print('내용1', '내용2', sep = ' 띄우기 ')
+print('내용1', '내용2', end = ' 문자열 끝\n', sep = ' 띄우기 ')
+print('내용1', '내용2', end = ' 문자열 끝\n', sep = ' 띄우기 ')
+# line 8, 9 처럼 end와 sep는 순서 상관 x
