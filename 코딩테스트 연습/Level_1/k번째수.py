9d834cbd6b15a1a98a9e40ec60243d09b032b0c0
RnL <63404135+tldnr1@users.noreply.github.com>
2022-04-26 13:49:27 +0000

    lv1. k번째수

--- 코딩테스트 연습/Level_1/k번째수.py
+++ 코딩테스트 연습/Level_1/k번째수.py
@@ -1,1 +1,22 @@
-print()
+def solution(array, commands):
+  answer = []
+  for command in commands:
+    # i : command[0] / j : command[1] / k : command[2]
+    sorted_arr = sorted(array[command[0]-1:command[1]])
+    answer.append(sorted_arr[command[2]-1])
+  return answer
+
+array = [1, 5, 2, 6, 3, 7, 4]
+commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+print(solution(array, commands))
+
+
+''' 모범 답안 '''
+"""
+def solution(array, commands):
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
+"""
+
+# map + lambda의 활용 설명 : https://tykimos.github.io/2020/01/01/Python_Lambda_Map/
+
+# 위 링크에 map, lambda 개별 설명도 있는데 확인해보기!
