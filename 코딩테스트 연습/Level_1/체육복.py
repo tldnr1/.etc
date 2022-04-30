4aa28160dca99171891d530130ff42d97be8dc15
RnL <63404135+tldnr1@users.noreply.github.com>
2022-04-30 06:08:47 +0000

    그리디 알고리즘

--- 코딩테스트 연습/Level_1/체육복.py
+++ 코딩테스트 연습/Level_1/체육복.py
@@ -1,6 +1,7 @@
 '''
 < 통과 못한 코드 >
 def solution(n, lost, reserve):
+  # reserve.sort() 추가하면 정답이 됨
   _reserve = [r for r in reserve if r not in lost]  # 여벌을 잃지 않은 학생
   _lost = [l for l in lost if l not in reserve]  # 여벌이 없으나 잃은 학생
 
@@ -12,6 +13,15 @@ def solution(n, lost, reserve):
     elif b in _lost:
       _lost.remove(b)
   return n - len(_lost)
+
+
+< 코드 분석 > 
+1. 아래는 set을 사용해서 set * for로 시간복잡도가 O(n)인 반면,
+    위의 코드는 for, lost로 인해 O(n^2)이다.
+
+2. reserve가 정렬되어 있지 않은 경우 틀리게 됨!
+    왜냐하면 Greedy 알고리즘에 의해 순서대로 앞 or 뒤에서 차례로 빌려줘야 하는데,
+    reserve가 정렬이 안되어있으면 막 빌려주게 되어서 최선의 결과가 안나옴
 '''
 
 def solution(n, lost, reserve):
