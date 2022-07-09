a51862cc7f1b517202005cb5a0fdba2fbbb06764
RnL <63404135+tldnr1@users.noreply.github.com>
2022-07-09 13:41:49 +0000

    update 369게임

--- 이것이 코딩테스트다/코드업_python기초100제/test.py
+++ 이것이 코딩테스트다/코드업_python기초100제/test.py
@@ -1,2 +1,6 @@
-a, b = map(bool, map(int, input().split()))
-print(a and b)
+n = int(input())
+for i in range(1, n+1):
+    if i%10 in [3,6,9]:
+        print('X', end=' ')
+    else:
+        print(i, end=' ')
