94a3df1ab6b100141aabde275cd1ed5173ea34e3
RnL <63404135+tldnr1@users.noreply.github.com>
2022-07-09 07:01:29 +0000

    update 삼항 연산자 a,b,c 크기비교

--- 이것이 코딩테스트다/코드업_python기초100제/삼항 연산자.py
+++ 이것이 코딩테스트다/코드업_python기초100제/삼항 연산자.py
@@ -3,3 +3,9 @@
 # b >= a 인 경우 True, 아니면 False
 a, b = map(int, input().split())
 print('True' if b >= a else 'False')
+
+# a, b, c 중 가장 작은 값 찾기
+# 알고리즘 : (a,b 중 작은값 고름) -> c랑 비교
+#             ㄴ (a,b중 작은값) if (a,b중 작은값)<c else c
+a, b, c = map(int, input().split())
+print((a if a<b else b) if (a if a<b else b)<c else c)
