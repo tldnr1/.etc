6b82528525517f5cb26b4b9c7f2a59f079d58e90
RnL <63404135+tldnr1@users.noreply.github.com>
2022-07-09 06:18:34 +0000

    update 소수점 자리 지정

--- 이것이 코딩테스트다/코드업_python기초100제/test.py
+++ 이것이 코딩테스트다/코드업_python기초100제/test.py
@@ -1,6 +1,2 @@
-# ord() : ordinary position 으로 아스키코드 값을 반환
-# chr() : character 으로 아스키코드 상의 문자를 반환
-
-# 입력 받은 문자의 다음 문자를 출력
-s = ord(input())
-print(chr(s+1))
+f1, f2 = map(float, input().split())
+print(round(f1/f2, 3))
