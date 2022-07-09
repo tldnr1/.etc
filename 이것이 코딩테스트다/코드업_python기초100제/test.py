6ac833689dbb75b004fd468b45987053c12ae3f1
RnL <63404135+tldnr1@users.noreply.github.com>
2022-07-09 14:14:42 +0000

    update

--- 이것이 코딩테스트다/코드업_python기초100제/test.py
+++ 이것이 코딩테스트다/코드업_python기초100제/test.py
@@ -1,9 +1,4 @@
-# round(값, 반올림위치) : 반올림, 단 자동으로 소수점 줄어들 수 있음 (1.000 => 1.0)
-# format(값, 반올림 틀) : 반올림 틀('.2f' 등)에 무조건 맞춰서 출력
-
-f1 = 10.0
-f2 = 0.01
-
-print(round(f1/f2, 3))
-print(format(f1/f2, '.3f'))
-print('{0:.3f}'.format(f1/f2))
+a, m, d, n = map(int, input().split())
+for _ in range(n-1):
+    a = a * m + d
+print(a)
