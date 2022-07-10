330e001776b90dc0ca532c5aa46ec16d67ed0aca
RnL <63404135+tldnr1@users.noreply.github.com>
2022-07-10 06:18:12 +0000

    update

--- 이것이 코딩테스트다/코드업_python기초100제/test.py
+++ 이것이 코딩테스트다/코드업_python기초100제/test.py
@@ -1,4 +1,22 @@
-a, m, d, n = map(int, input().split())
-for _ in range(n-1):
-    a = a * m + d
-print(a)
+box = [[0]*11 for _ in range(11)]
+for i in range(1,11):
+    line = list(map(int, input().split()))
+    box[i][1:] = line
+    
+x, y = 2, 2
+while 1:
+    if box[x][y] == 2:
+        box[x][y] = 9
+        break
+    
+    if box[x][y+1] == 1 and box[x+1][y] == 1:
+        break
+    
+    box[x][y] = 9
+    if box[x][y+1] == 1: x += 1
+    else: y += 1
+
+for i in range(1,11):
+  for j in range(1,11):
+    print(box[i][j], end=' ')
+  print()
