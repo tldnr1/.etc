e73b0f3fcb4e8cf60736834a76fb61f3bd600190
RnL <63404135+tldnr1@users.noreply.github.com>
2022-07-16 14:37:47 +0000

    아시아 정보올림피아드

--- 이것이 코딩테스트다/코드업/정올 1번 수준 문제/아시아 정보올림피아드.py
+++ 이것이 코딩테스트다/코드업/정올 1번 수준 문제/아시아 정보올림피아드.py
@@ -1,3 +1,7 @@
+# 순위 정하기 등 주어진 정보로 순번을 결정해야하면
+# 무작정 1,2,3 등 판별하는 코드를 짜지 말고
+# 1, 2등처럼 앞쪽 순서는 '확정적으로 정해지는 경우'가 있는지 확인해보기
+
 n = int(input())
 participant = []
 for _ in range(n):
@@ -6,7 +10,7 @@ for _ in range(n):
 
 participant.sort(key = lambda x: x[2], reverse=True)
 
-# 금메달, 은메달 출력
+# 금메달, 은메달 출력  -> 금, 은메달은 확정적임!
 print(participant[0][0], participant[0][1])
 print(participant[1][0], participant[1][1])
 
