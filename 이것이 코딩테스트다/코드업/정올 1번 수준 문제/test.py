d98a5dbb1ca6e0475f7cb336bc67a38dda708bc0
RnL <63404135+tldnr1@users.noreply.github.com>
2022-07-17 06:53:43 +0000

    update 정올 1번

--- 이것이 코딩테스트다/코드업/정올 1번 수준 문제/test.py
+++ 이것이 코딩테스트다/코드업/정올 1번 수준 문제/test.py
@@ -1,12 +1,33 @@
-n, k = map(int, input().split())
-t = list(map(int, input().split()))
+score = [0, 0, 0]
+p = [[0]*3 for _ in range(3)]
 
-t_sum = sum(t[:k])
-t_max = t_sum
+n = int(input())
+for _ in range(n):
+  x = list(map(int,input().split()))
 
-for i in range(k, n):
-  t_sum += t[i] - t[i-k]
-  if t_max < t_sum:
-    t_max = t_sum
-    
-print(t_max)
+  for i in range(3):
+    score[i] += x[i]
+    p[i][x[i]-1] += 1
+
+max_score = max(score)
+max_cnt = 0
+for i in range(3):
+  if max_score == score[i]:
+    max_cnt += 1
+
+if max_cnt == 1:
+  print(score.index(max_score)+1, max_score)
+elif max_cnt == 2:
+  p3 = []
+  for i in range(3):
+    if max_score == score[i]:
+      p3.append((i+1, p[i][2]))
+
+  if p3[0][1] > p3[1][1]:
+    print(p3[0][0], max_score)
+  elif p3[0][1] < p3[1][1]:
+    print(p3[1][0], max_score)
+  else:
+    print(0, max_score)
+else:
+  print(0, max_score)
