7c878225ae544c0a85e2cf1334f7cc3f1dade9af
RnL <63404135+tldnr1@users.noreply.github.com>
2022-08-02 13:30:00 +0000

    update ReadMe 220802

--- 이것이 코딩테스트다/코드업/관리자 추천 문제/test.py
+++ 이것이 코딩테스트다/코드업/관리자 추천 문제/test.py
@@ -1,4 +1,15 @@
-a, b = map(int, input().split())
-max_l = -1e9
-l = 0
+def SuperSum(k, n):
+  if k == 0:
+    return n
+  else:
+    superSum = 0
+    for i in range(1,n+1):
+      superSum += SuperSum(k-1,i)
+    return superSum
 
+while(True):
+  try:
+    k, n = map(int, input().split())
+    print(SuperSum(k,n))
+  except:
+    break
