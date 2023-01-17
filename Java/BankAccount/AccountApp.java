a796a4e1804e15efedcdbd747f294cc6da1120c2
RnL <63404135+tldnr1@users.noreply.github.com>
2023-01-17 23:18:39 +0900

    AccountApp.java 기본 틀 완성
    1 if-else 대신 switch-case문 사용
    2. 실행 종료를 확실히 알아볼 수 있게 run을 boolean형으로 지정
    3. try-catch의 예외를 InputMismatchException으로 특정
    4. Account 클래스에서 사용할 메소드 미리 정리

--- Java/BankAccount/AccountApp.java
+++ Java/BankAccount/AccountApp.java
@@ -1,45 +1,56 @@
 // https://nackwon.tistory.com/100 보고 기능 구현하는 중
+// 자주 발생하는 예외 : https://best421.tistory.com/10\
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main
 {
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
-	    int num=0;
-	    do {
+	    boolean run = true;  // 실행 중지를 명확하게 표현하기 위해 사용
+	    int money = 0;
+	    
+	    // 임의 계좌 1개 생성
+	    // Account account = new Account(account_num, money);
+	    while (run) {
 	        System.out.println("-------------------------------------");
-	        System.out.println("1.예금");
-	        System.out.println("2.출금");
-	        System.out.println("3.잔고");
-	        System.out.println("4.종료");
-	        System.out.print("원하는 기능의 번호를 입력해주세요 >> ");
+	        System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
+	        System.out.println("-------------------------------------");
+	        System.out.print("선택>> ");
 	        
 	        try {
-	            num = sc.nextInt();
-	            switch (num) {
+	            int menuNo = sc.nextInt();
+	            if (menuNo<1 || menuNo>4) {
+	                System.out.println("범위 내에서 선택해주세요.");
+	            }
+	            
+	            switch (menuNo) {
 	                case 1:
 	                    System.out.println("1번-예금을 선택");
+	                    //account.deposit(money);
 	                    break;
 	                case 2:
 	                    System.out.println("2번-출금을 선택");
+	                    //account.withdraw(money);
 	                    break;
 	                case 3:
 	                    System.out.println("3번-잔고를 선택");
+	                    //account.showBalance(money);
 	                    break;
 	                case 4:
 	                    System.out.println("4번-종료를 선택");
+	                    run = false;
 	                    break;
 	                default:
-	                    System.out.println("-------------------------------------");
 	                    break;
 	            }
-	        }catch (Exception e) {
+	        }catch (InputMismatchException e) {
 	            System.out.println("정수를 입력해주세요.");
 	            sc.nextLine();  // 버퍼 비우기
 	            continue;
 	        }finally {
 	            System.out.println();
 	        }
-	    }while(num!=4);
+	    }
 	}
 }
