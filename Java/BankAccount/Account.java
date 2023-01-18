8f2e0cee4a30e1ef7a42e24fa2539d9572432d72
RnL <63404135+tldnr1@users.noreply.github.com>
2023-01-18 22:32:44 +0900

    Update Account.java
    출력문 추가

--- Java/BankAccount/Account.java
+++ Java/BankAccount/Account.java
@@ -1,21 +1,22 @@
-public class Account
-{
+public class Account {
     // 외부에서 변경되면 안되기 때문에 private으로 작성
     private String accountNo;  // 계좌번호
-    private int balance;  // 잔고
-    
+    private int balance = 0;  // 잔고
+
     public Account(String accountNo) {
         this.accountNo = accountNo;
     }
-    
+
     public void deposit(int money) {
         balance = balance + money;
+        System.out.println("예금액 : " + money);
     }
-    
+
     public void withdraw(int money) {
         balance = balance - money;
+        System.out.println("출금액 : " + money);
     }
-    
+
     public void showBalance() {
         System.out.println("잔고 : " + balance);
     }
