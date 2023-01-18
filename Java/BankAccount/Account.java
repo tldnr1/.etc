0a93960103e8cf7527cdc69c8264f774036d23fc
RnL <63404135+tldnr1@users.noreply.github.com>
2023-01-18 22:11:54 +0900

    Create Account.java

--- Java/BankAccount/Account.java
+++ Java/BankAccount/Account.java
@@ -1,1 +1,22 @@
-// https://nackwon.tistory.com/100 보고 기능 구현하는 중
+public class Account
+{
+    // 외부에서 변경되면 안되기 때문에 private으로 작성
+    private String accountNo;  // 계좌번호
+    private int balance;  // 잔고
+    
+    public Account(String accountNo) {
+        this.accountNo = accountNo;
+    }
+    
+    public void deposit(int money) {
+        balance = balance + money;
+    }
+    
+    public void withdraw(int money) {
+        balance = balance - money;
+    }
+    
+    public void showBalance() {
+        System.out.println("잔고 : " + balance);
+    }
+}
