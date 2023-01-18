6387fe0ffdba191a313585402e349d2ebbe59fd2
RnL <63404135+tldnr1@users.noreply.github.com>
2023-01-18 22:42:18 +0900

    Update Account.java

--- Java/BankAccount/Account.java
+++ Java/BankAccount/Account.java
@@ -1,4 +1,4 @@
-public class Account {
+public class Account {  // 파일 1개에서 실행하려면 main을 제외하고는 class앞에 public을 지워야 함!
     // 외부에서 변경되면 안되기 때문에 private으로 작성
     private String accountNo;  // 계좌번호
     private int balance = 0;  // 잔고
